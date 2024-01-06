4c3724846a12a77e0160584bf0a99f210f22f456
Siddhant <patelsiddhant9297@gmail.com>
2024-01-06 21:07:51 +0530

    122:Create Post Features

---
+++ src/main/java/com/blog/myFirstBlog/MyFirstBlogApplication.java
@@ -0,0 +1,13 @@
+package com.blog.myFirstBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/myFirstBlog/MyFirstBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.myFirstBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
